4fd2bd2ed9f4bed785c1f2a40d599c5d5fd9dccd
EvaSax99 <evaclement99@gmail.com>
2026-03-29 17:54:39 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+
+RouterView
+
+</script>
+
+<template>
+
+  <main>
+   <RouterView />
+  </main>
+
+  
+</template>
---
+++ src/paginae/responsum/Responsum.vue
@@ -0,0 +1,6 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+    <h1>Indecision Page</h1>
+</template>
