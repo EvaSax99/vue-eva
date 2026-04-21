dd73dec4494eb9fece08219abe536b4114b1bdbd
EvaSax99 <evaclement99@gmail.com>
2026-04-21 17:22:23 +0200

    evclesa: Página de inicio Simpsons

--- src/paginae/simpsons/Primus.vue
+++ src/paginae/simpsons/Primus.vue
@@ -1,9 +1,36 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { RouterLink } from 'vue-router';
+import {Button} from '@/components/ui/button';
+import LogoSimpsons from '@/components/LogoSimpsons.vue';
+
+</script>
 
 <template>
-  <h1
-    class="text-6xl m-8 text-red-500 bg-amber-300 w-fit px-12 py-5 rounded-xl shadow-xl hover:bg-red-500 hover:text-white"
-  >
-    Simpsons Page
-  </h1>
+<div class="bg-[#ffde00] min-h-screen text-center p-4 flex flex-col items-center justify-center">
+<LogoSimpsons estilus="w-[24rem]"  />
+	<h1 class="text-xl my-1 font-medium">
+    	Discover some of the most amazing Quotes from The Simpsons
+	</h1>
+	<div>
+    	<strong>This is an unofficial app (even though it looks so cool!)</strong>
+	</div>
+	<div className="mt-8 flex gap-5">
+		<Button variant="simpsonsfont" size="elongatis">
+      		<RouterLink to="/">Exit</RouterLink>
+    	</Button>
+    	<Button variant="simpsonsfont" size="elongatis">
+      		<RouterLink to="">Enter</RouterLink>
+    	</Button>
+	</div>
+	<div class="flex gap-12 mt-8">
+		<img class="object-contain hidden md:block" src="/imagines/simpsons/bart.png" alt="Bart Simpson" width="200px">
+		<img class="object-contain hidden md:block mt-20" src="/imagines/simpsons/homer.png" alt="Homer Simpson" width="200px">
+		<img class="object-contain hidden md:block mt-0" src="/imagines/simpsons/moe.png" alt="Moe Szyslak" width="200px">
+
+
+</div>
+
+</div>
+
+
 </template>
